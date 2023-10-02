2336fde10499997d6958355588247e00cbb23a04
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-10-02 09:44:22 +0600

    firebase config

---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDl588Kehhk-DgMwhgr6Z1ViVmAgYxNino",
+  authDomain: "lotus-life-yoga.firebaseapp.com",
+  projectId: "lotus-life-yoga",
+  storageBucket: "lotus-life-yoga.appspot.com",
+  messagingSenderId: "287330412735",
+  appId: "1:287330412735:web:6ffb691c71990032fe3e0a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
